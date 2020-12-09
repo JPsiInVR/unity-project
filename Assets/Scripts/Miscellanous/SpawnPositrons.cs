fe36b398855aedce98b147b7b3244957db4557c8
Robert Skrzypczak <robert.skrzypczak21@gmail.com>
2020-12-09 14:40:37 +0100

    Added Lepton Storage Room scene with assets

---
+++ Assets/Scripts/Miscellanous/SpawnPositrons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPositrons : MonoBehaviour
+{
+    private float radius = 9f;
+    
+    [SerializeField]
+    GameObject positron;
+    [SerializeField]
+    GameObject shelvesCenter;
+
+    private GameObject particle;
+
+    private float particlesCount = 54;
+    private float angle = 0;
+    private Vector3 newPos;
+    
+    void Start()
+    {
+        for (int i = 0; i < particlesCount; i++)
+        {
+            angle = i * Mathf.PI*2f / particlesCount;
+            newPos = new Vector3(Mathf.Cos(angle)*radius + shelvesCenter.transform.position.x, 0.8f + this.transform.position.y, Mathf.Sin(angle)*radius + shelvesCenter.transform.position.z);
+            particle = Instantiate(positron, newPos, Quaternion.Euler(0f, -i*Mathf.PI*2.12f, 0f));
+            particle.transform.SetParent(this.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/PositronMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositronMovement : MonoBehaviour
+{
+    [SerializeField]
+    GameObject electron;
+
+    [SerializeField]
+    GameObject explosion;
+
+    [SerializeField]
+    GameObject jpsi;
+
+    private float speed = 0.5f;
+
+    void Update()
+    {
+        speed *= 1.02f;
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, electron.transform.position, step);
+        if (Vector3.Distance(transform.position, electron.transform.position) < 0.001f)
+        {
+            electron.transform.position *= -1.0f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == electron.name){
+            StartCoroutine(SpawnJpsi());
+            Instantiate(explosion, new Vector3(5, 1, 0), Quaternion.identity);
+            electron.GetComponent<Renderer>().enabled = false;
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
+    }
+
+    IEnumerator SpawnJpsi(){
+        yield return new WaitForSeconds(2.0f);
+        GameObject jpsiObject = Instantiate(jpsi, new Vector3(5, 0.2f, 0), Quaternion.Euler(-90, 90, 0));
+        jpsiObject.transform.localScale = new Vector3(25f, 25f, 25f);
+        Destroy(gameObject);
+        Destroy(electron);
+    }
+}
