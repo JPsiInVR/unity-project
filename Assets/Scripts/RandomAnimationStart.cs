e1df3ebffa6d5f8d184cd27a5f7c6eb2ed4ef908
Adrian Gaborek <adriangaborek3@gmail.com>
2020-12-09 01:03:35 +0100

    Increase shadow render distance, randomize particles animation, add physics to banner, change voicover to the correct one.

---
+++ Assets/Scripts/RandomAnimationStart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RandomAnimationStart : MonoBehaviour
+{
+    private Animator Animator { get; set; }
+    private AnimatorStateInfo AnimatorStateInfo { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+        AnimatorStateInfo = Animator.GetCurrentAnimatorStateInfo(0);
+        Animator.speed = Random.Range(.7f, 1.3f);
+        Animator.Play(AnimatorStateInfo.fullPathHash, -1, Random.Range(0f, 1f));
+    }
+}
