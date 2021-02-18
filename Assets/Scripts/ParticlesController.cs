6796386be6e68030b36742acdc638e90440f1254
Adrian Gaborek <adriangaborek3@gmail.com>
2021-02-18 12:30:14 +0100

    Add music, camera shake, recorder settings, update packages

---
+++ Assets/Scripts/ParticlesController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ParticlesController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject electron;
+
+    [SerializeField]
+    GameObject explosion;
+
+    [SerializeField]
+    SkinnedMeshRenderer jpsi;
+
+    private bool isJpsiEnabled;
+
+    private void Start()    {
+        isJpsiEnabled = false;
+    }
+
+    public void ToggleParticles()
+    {
+        electron.GetComponent<Renderer>().enabled = isJpsiEnabled;
+        gameObject.GetComponent<Renderer>().enabled = isJpsiEnabled;
+        jpsi.enabled = !isJpsiEnabled;
+        isJpsiEnabled = !isJpsiEnabled;
+    }
+
+    public void EnableExplosion()
+    {
+        explosion.SetActive(true);
+    }
+}
