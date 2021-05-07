d5c6c4790631b34a3e56adcc1bbb7514e8591790
Adrian Gaborek <adriangaborek3@gmail.com>
2021-05-07 22:47:03 +0200

    Setup for cloud build
    * Convert .blend files to .fbx
    * Remove unused sample scenes and use scene provide by google instead
    * Switch to universal rendering pipeline with plans to also use HDRP.
    * Install shader graph
    * Install xr framework
    * Setup project for android builds
    * Add animated text which will be used for items descriptions
    * Update packages
    * Add gaze interactivity ( outline, selection, radial progress ) along witch touch input
    * Reorganize project structure
    * Enable cloud build
    * Switch to unity 2021.1.6
    * Add screen with particle list

---
+++ Assets/Scripts/Cardboard/CardboardInteractable.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CardboardInteractable : MonoBehaviour
+{
+    public float gazeTime;
+    public UnityEvent onHoverEnter = new UnityEvent();
+    public UnityEvent onHoverExit = new UnityEvent();
+    public UnityEvent onSelectEnter = new UnityEvent();
+    public UnityEvent onSelectExit = new UnityEvent();
+
+    private float _gazeStartTime;
+    private bool _isHovering;
+    private bool _isSelecting;
+
+    public void PointerEnter()
+    {
+        _isHovering = true;
+        _gazeStartTime = Time.time;
+        RadialProgress.Instance.PlayDefault();
+        onHoverEnter.Invoke();
+    }
+
+    public void PointerExit()
+    {
+        _isHovering = false;
+        _gazeStartTime = -1;
+        RadialProgress.Instance.PlayFadeOut();
+        onHoverExit.Invoke();
+    }
+
+    private void Update()
+    {
+        if (XRCardboardController.Instance.IsTriggerPressed())
+        {
+            HandleInput();
+        }
+
+        if(_isHovering && _gazeStartTime >= 0)
+        {
+            if (Time.time - _gazeStartTime > gazeTime)
+            {
+                RadialProgress.Instance.Progress = 1;
+                RadialProgress.Instance.PlayFadeOut();
+                HandleInput();
+            }
+            else
+            {
+                RadialProgress.Instance.Progress = (Time.time - _gazeStartTime) / gazeTime;
+            }
+        }
+    }
+
+    private void HandleInput()
+    {
+        if (_isSelecting)
+        {
+            _gazeStartTime = -1;
+            _isSelecting = false;
+            onSelectExit.Invoke();
+        }
+        else if (_isHovering)
+        {
+            _gazeStartTime = -1;
+            _isSelecting = true;
+            onSelectEnter.Invoke();
+        }
+    }
+}
